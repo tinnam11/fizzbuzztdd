8fc714cbb32bd19d371ef7ae925cba3cda5af6e9
tinnam <uw_tinna.mokaramanee@krungthai.com>
2024-07-10 20:05:06 +0700

    test 8 pass

--- fizzbuzz1/fizzbuzz1.go
+++ fizzbuzz1/fizzbuzz1.go
@@ -1,20 +1,13 @@
 package fizzbuzz
 
+import "strconv"
+
 func fizzBuzz(num int) string {
-	if num == 7 {
-		return "7"
-	}
 	if num == 5 {
 		return "Buzz"
-	}
-	if num == 4 {
-		return "4"
 	}
 	if num%3 == 0 {
 		return "Fizz"
 	}
-	if num == 2 {
-		return "2"
-	}
-	return "1"
+	return strconv.Itoa(num)
 }
