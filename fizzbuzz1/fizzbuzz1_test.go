9c6ca3178cabba6b2078aa6622a52611b74fcfbe
tinnam <uw_tinna.mokaramanee@krungthai.com>
2024-07-10 19:53:47 +0700

    test 2

--- fizzbuzz1/fizzbuzz1_test.go
+++ fizzbuzz1/fizzbuzz1_test.go
@@ -10,3 +10,12 @@ func TestOne(t *testing.T) {
 		t.Errorf("given num (%d) expect %q actual %q\n", num, expect, actual)
 	}
 }
+
+func TestTwo(t *testing.T) {
+	num := 2
+	expect := "2"
+	actual := fizzBuzz(num)
+	if expect != actual {
+		t.Errorf("given num (%d) expect %q actual %q\n", num, expect, actual)
+	}
+}
